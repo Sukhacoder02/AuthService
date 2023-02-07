dff918fb4ad656ee157708cd4674ef01a6d3521d
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-02-07 15:24:38 +0530

    chore: initialise the project

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// require express
+import express from 'express';
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is  listening at http://localhost:${port}`);
+});
