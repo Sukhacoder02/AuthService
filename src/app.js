a2e192c451c92a3d84d7b3419c3c39569f12ced2
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-03-07 22:43:56 +0530

    feat: add cors in app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 // require express
 const express = require('express');
 const port = 3000;
+const cors = require('cors');
 const router = require('./routes/routes');
 // require dotenv
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: 'http://localhost:3001',
+  })
+);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
